b7f1e2971bf6eee0920a8384381a24742a14ae72
rabinauget <rabinauget@gmail.com>
2024-12-19 16:52:31 +0300

    my first commit

---
+++ TP-2/ec2.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.81.0"
+    }
+  }
+  required_version = "v1.10.3"
+}
+
+provider "aws" {
+  region = "us-east-1"
+  access_key = AKIA3FLD57ASU5OLYM4X
+  secret_key = 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
+}
+
+resource "aws_instance" "myec2" {
+    ami = ami-005fc0f236362e99f
+    instance_type = "t2.micro"
+    key_name = "terraform-training"
+    tags = {
+        Name = "ec2-training-terraform"
+    } 
+}
