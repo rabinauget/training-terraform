4f5b837beab0a8b7cb6922c93deb2d9f09703a05
rabinauget <rabinauget@gmail.com>
2024-12-27 20:53:43 +0300

    add TP-4

---
+++ TP-4/main.tf
@@ -0,0 +1,91 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.81.0"
+    }
+  }
+  required_version = "v1.10.3"
+}
+
+provider "aws" {
+  region                   = "us-east-1"
+  shared_credentials_files = ["../.secrets/credentials"]
+  profile                  = "default"
+}
+
+data "aws_ami" "app_ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
+resource "aws_instance" "myec2" {
+  ami             = data.aws_ami.app_ami.id
+  instance_type   = var.instance_type
+  key_name        = "terraform-training"
+  tags            = var.aws_common_tag
+  security_groups = [aws_security_group.allow_http_https_ssh.name]
+
+  provisioner "local-exec" {
+    command = "echo PUBLIC IP: ${self.public_ip} ID: ${aws_instance.myec2.id} AZ: ${aws_instance.myec2.availability_zone} > infos_tp4_ec2.txt"
+  }
+
+  provisioner "remote-exec" {
+    inline = [ 
+      "sudo amazon-linux-extras install -y nginx",
+      "sudo systemctl start nginx"
+    ]
+    connection {
+      type = "ssh"
+      user = "ec2-user"
+      private_key = file("../.secret/terraform-training.pem")
+      host = self.public_ip
+    }
+  }
+}
+
+resource "aws_security_group" "allow_http_https_ssh" {
+  name        = "training-sg"
+  description = "Allow http, https and ssh inbound traffic"
+
+  ingress {
+    description = "http from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "https from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "ssh from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "allow all traffic from VPC"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_eip" "load_balancer" {
+  instance = aws_instance.myec2.id
+}
