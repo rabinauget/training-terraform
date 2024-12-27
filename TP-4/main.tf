7b378770544413a411ad9b6a6b7ad2f74a9912a6
root <rabinauget@gmail.com>
2024-12-27 19:37:59 +0100

    correct local exec placement

--- TP-4/main.tf
+++ TP-4/main.tf
@@ -86,6 +86,6 @@ resource "aws_eip" "load_balancer" {
   instance = aws_instance.myec2.id
 
   provisioner "local-exec" {
-    command = "echo PUBLIC IP: ${aws_eip.load_balancer.public_ip}  ID: ${aws_instance.myec2.id}  AZ: ${aws_instance.myec2.availability_zone} > infos_ec2.txt"  
+    command = "echo PUBLIC IP: ${aws_eip.load_balancer.public_ip} ID: ${aws_instance.myec2.id} AZ: ${aws_instance.myec2.availability_zone} > infos_ec2.txt"  
   }
 }
